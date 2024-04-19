494b9dd653c38697ec7c57dca1805e0751e91702
lkosters <laurens@laurenskosters.com>
2024-04-19 23:38:22 +0200

    Refactor Header and Hero Vue components
    Removed unnecessary comments from the Header.vue component to improve clarity of code. Also, applied a padding adjustment in Hero.vue to allow better responsiveness on larger screens. These changes contribute to cleaner and more maintainable code base.

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="py-10">
-    <div class="max-w-screen-xl mx-auto">
+    <div class="max-w-screen-xl px-5 lg:px-0 mx-auto">
       <h1 class="font-bold text-4xl">Share your code snippets</h1>
       <div class="mockup-code my-8">
         <textarea class="textarea textarea-ghost w-full bg-transparent !outline-none !border-none min-h-96" placeholder="your code"></textarea>
