7784aeb3ef94a7d2a9425174a31d492998f5b446
lkosters <laurens@laurenskosters.com>
2024-04-19 23:25:52 +0200

    Created landing page, installed tailwindCSS and DaisyUI

---
+++ components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer class="footer footer-center p-4 bg-base-300 text-base-content">
+    <aside>
+      <p>Made by <NuxtLink target="_blank" to="https://laurenskosters.nl/">Laurens Kosters</NuxtLink></p>
+    </aside>
+  </footer>
+</template>
